d8fa91e247dda66a777ea3b92949ef4e1ade25ee
User Name <user@name.com>
2022-04-05 11:31:01 +0000

    initial commit

---
+++ src/main/java/com/nimbleways/Program.java
@@ -0,0 +1,7 @@
+package com.nimbleways;
+
+class Program {
+    public static void main() {
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/java/com/nimbleways/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.nimbleways;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SimpleTest {
+
+    @Test
+    void foo() {
+        assertTrue(true);
+    }
+
+}
