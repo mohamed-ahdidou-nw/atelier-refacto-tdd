bfd6f1cb025ff7d749825e15078e8938262d3998
Noureddine Achibane <nachibane27@gmail.com>
2022-04-06 15:28:55 +0000

    Initial

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,8 @@
 package com.gildedrose;
 
+import java.util.ArrayList;
+import java.util.Dictionary;
+
 class GildedRose {
     Item[] items;
 
@@ -8,30 +11,22 @@ class GildedRose {
     }
 
     public void updateQuality() {
+        ArrayList<String> itemSpec = new ArrayList<>();
+        itemSpec.add("Aged Brie");
+        itemSpec.add("Backstage passes to a TAFKAL80ETC concert");
+        itemSpec.add("Sulfurs, Hand of Ragnaros");
         for (Item item : items) {
-            if (!"Aged Brie".equals(item.name)
-                && !"Backstage passes to a TAFKAL80ETC concert".equals(item.name)) {
-                if (item.quality > 0) {
-                    if (!"Sulfurs, Hand of Ragnaros".equals(item.name)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(item.name)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+            if ((item.quality > 0) && !itemSpec.contains(item.name)) {
+                item.quality = item.quality - 1;
+            } else if (item.quality < 50) {
+                item.quality = item.quality + 1;
+                if ("Backstage passes to a TAFKAL80ETC concert".equals(item.name)
+                    && (item.sellIn < 11)
+                    && (item.quality < 50)) {
+                    if (item.sellIn >= 6) {
+                        item.quality = item.quality + 1;
+                    } else {
+                        item.quality = item.quality + 2;
                     }
                 }
             }
@@ -42,20 +37,17 @@ class GildedRose {
 
             if (item.sellIn < 0) {
                 if (!"Aged Brie".equals(item.name)) {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.name)) {
-                        if (item.quality > 0) {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(item.name)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
+                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.name)
+                        && !"Sulfuras, Hand of Ragnaros".equals(item.name)
+                        && item.quality > 0) {
+                        item.quality = item.quality - 1;
                     } else {
                         item.quality = 0;
                     }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                } else if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
+
             }
         }
     }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,10 +8,24 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 0) ,
+                                    new Item("Sulfuras, Hand of Ragnaros",10,50)};
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+
+       /* assertEquals("Aged Brie", app.items[0].name);
+        assertEquals(2, app.items[0].quality);
+        assertEquals(49, app.items[1].quality);
+*/
+        shouldBe20after10days(app,app.items[0]);
+
+        assert(app.items[0].quality <= 50);
+    }
+
+    void shouldBe20after10days(GildedRose app,Item item) {
+        for (int i=0;i<10;i++){
+            app.updateQuality();
+        }
+        assertEquals(20, item.quality);
     }
 
 }
