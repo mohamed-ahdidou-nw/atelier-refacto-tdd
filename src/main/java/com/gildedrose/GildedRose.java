b94cca85c385ef87c85e5c4b642d7bb6cf361ab8
MaryamKhallala <Maryam.khallala@etu.fstm.ac.ma>
2022-04-06 15:31:23 +0000

    MaryamKhallala

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,54 +7,55 @@ class GildedRose {
         this.items = items;
     }
 
+    //in order to refactoring i use othor methodes to simplify the code
+    /* dans ce code là j'ai utiliser la notion de incrementation et décrementation au lieu de faire manuellement*/
+    /* aussi j'ai combiner les condition pour etre mieux lisible au lieu de faire a chaque fois if else */
+
+    public void QualityUnderFifty(){
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].quality < 50) {
+                items[i].quality ++;
+            }
+        }
+    }
+public void MinceQualityIfIsPositive() {
+    for (int i = 0; i < items.length; i++) {
+        if (items[i].quality > 0) {
+            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                items[i].quality --;
+            }
+        }
+    }
+}
+
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+                MinceQualityIfIsPositive();
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                QualityUnderFifty();
+                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")&&(items[i].sellIn < 11)) {
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
+                           QualityUnderFifty();
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                            QualityUnderFifty();
                         }
                     }
                 }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                items[i].sellIn --;
             }
 
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
+                        MinceQualityIfIsPositive();
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].quality = 0;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    QualityUnderFifty();
                 }
             }
         }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,28 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+
+    }
+//teste unitaire permetre de tester si le nom de item apres update equal Aged Brie
+
+    @Test
+    void TestNameAfterUpdatEqualAgedBrie() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie", app.items[0].name);
+
+    }
+    //teste unitaire permetre de tester si la quality de item apres update ajouter 2 sur l'existant
+
+    @Test
+    void TestQualityAfterUpdatIncrementByTwo() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(2, app.items[0].quality);
+
     }
 
 }
