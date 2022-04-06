552286d6fc6c5381818b81c6bcea93f021870e37
Chaimae Bougattaya <chaimaebougattaya1@gmail.com>
2022-04-06 15:44:25 +0000

    chaimaecommit

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,11 +2,42 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
-
+    private final String sulfuras = "Sulfuras, Hand of Ragnaros";
+    private final String agedBrie = "Aged Brie";
+    private final String backstage = "Backstage passes to a TAFKAL80ETC concert";
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
+    public boolean itemNameNotAgedBrie(Item item)
+    {
+        return !item.name.equals(agedBrie);
+    }
+    public boolean itemNameNotBackstagePasses(Item item)
+    {
+        return !item.name.equals(backstage);
+    }
+    public boolean itemNameNotSulfuras(Item item)
+    {
+        return !item.name.equals(sulfuras);
+    }
+    public boolean itemQualitySup0(Item item)
+    {
+        return (item.quality>0);
+    }
+    public void refactoringUpdateQuality(){
+        for(Item it : items)
+        {
+            if(itemNameNotAgedBrie(it) && itemNameNotBackstagePasses(it))
+            {
+                if(itemQualitySup0(it))
+                {
+
+                }
+
+            }
+        }
+    }
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,37 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    void qualityIsNeverNegative(){
+        Item[] items = new Item[] { new Item("foo", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        for(int i=0 ;i<12;i++)
+            app.updateQuality();
+        assert(app.items[0].quality>=0);
+    }
+
+    @Test
+    void qualityIsNeverMore50(){
+        Item[] items = new Item[] { new Item("foo", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        for(int i=0 ;i<12;i++)
+            app.updateQuality();
+        assert(app.items[0].quality<50);
+    }
+    @Test
+    void nameIsSulfurasNotSelledQualityNotDecrease(){
+        Item[] items = new Item[] { new Item("foo", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        int q = app.items[0].quality;//save the quality of the item
+
+        assertEquals("Sulfuras",app.items[0].name);
+        assert(app.items[0].sellIn==0);//if item not selled (sellIn=0)
+        assert (app.items[0].quality>=q);//if quality not decrease
     }
 
 }
+
