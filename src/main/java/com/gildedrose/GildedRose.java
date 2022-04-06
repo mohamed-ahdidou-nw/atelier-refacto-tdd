9e99edddf15bd9775bb271ec0a65054bf09a891e
AyoubAHBACHOU <ayoub.ahbachou-etu@etu.univh2c.ma>
2022-04-06 15:47:40 +0100

    AHBACHOU AYOUB Solution

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,13 +10,15 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")
+                    ) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
-            } else {
+            }
+            else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,67 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
+    @Test
+    void QualityNeg() {
+    	Item[] items= new Item[]{new Item("Aged Brie",0,0)};
+    	GildedRose app = new GildedRose(items);
+    	app.updateQuality();
+    	assert(app.items[0].quality>=0);
+    	
+    }
+    
+    @Test
+    void Quality_NotNeg_AllItems() {
+    	Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6) };
+    	GildedRose app = new GildedRose(items);
+    	app.updateQuality();
+    	for(Item it:items) {
+    		assert(it.quality>=0);
+    	}
+    }
+    	
+    	  @Test
+    	    void Quality_NotMore50_AllItems() {
+    	    	Item[] items = new Item[] {
+    	                new Item("+5 Dexterity Vest", 10, 20), //
+    	                new Item("Aged Brie", 2, 0), //
+    	                new Item("Elixir of the Mongoose", 5, 7), //
+    	                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+    	                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    	                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    	                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+    	                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+    	                // this conjured item does not work properly yet
+    	                new Item("Conjured Mana Cake", 3, 6) };
+    	    	GildedRose app = new GildedRose(items);
+    	    	app.updateQuality();
+    	    	for(Item it:items) {
+    	    		assert(it.quality<=50);
+    	    	}
+    	
+    	
+    }
+    	  @Test
+    	  void SulfurasSellIn() {
+    	        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+    	        GildedRose app = new GildedRose(items);
+    	        app.updateQuality();
+    	        assert(items[0].sellIn==80);
+    	    }
+    	  
+    	  
+    
 
 }
